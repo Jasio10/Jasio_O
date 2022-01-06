14358ef6764ee67bbcfcbeabfeacc63a960e10e8
Jasio10 <jasio10@yahoo.com>
2022-01-06 12:32:20 -0600

    abc

--- Pierwszy.py
+++ Pierwszy.py
@@ -1,5 +1,14 @@
 print( "Hello Jasio gluptasio")
 print("Jan 6")
+<<<<<<< Updated upstream
 print("A")
 print("B")
 print("C")
+=======
+<<<<<<< HEAD
+=======
+print("A")
+print("B")
+print("C")
+>>>>>>> pierwszy---test1
+>>>>>>> Stashed changes
